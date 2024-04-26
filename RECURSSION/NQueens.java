a3eb09bf1d6c3d4536e8b92fc28ba92b528e1929
narendra_kamepalli <nari73092@gmail.com>
2024-04-26 21:49:12 +0530

    first commit

---
+++ RECURSSION/NQueens.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NQueens {
+    public boolean isSafe(int row, int col, char[][] board) {
+        // Check upper columns in the same row
+        for (int j = 0; j < col; j++) {
+            if (board[row][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check upper diagonal on left side
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check lower diagonal on left side
+        for (int i = row, j = col; i < board.length && j >= 0; i++, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void saveBoard(char[][] board, List<List<String>> allBoards) {
+        List<String> neboard = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            StringBuilder row = new StringBuilder();
+            for (int j = 0; j < board[0].length; j++) {
+                row.append(board[i][j]);
+            }
+            neboard.add(row.toString());
+        }
+        allBoards.add(neboard);
+    }
+
+    public void helper(char[][] board, List<List<String>> allBoards, int col) {
+        if (col == board.length) {
+            saveBoard(board, allBoards);
+            return;
+        }
+        for (int row = 0; row < board.length; row++) {
+            if (isSafe(row, col, board)) {
+                board[row][col] = 'Q';
+                helper(board, allBoards, col + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> allBoards = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (char[] row : board) {
+            Arrays.fill(row, '.');
+        }
+        helper(board, allBoards, 0);
+        return allBoards;
+    }
+
+    public static void main(String[] args) {
+        NQueens nQueens = new NQueens();
+        List<List<String>> solutions = nQueens.solveNQueens(4);
+        for (List<String> solution : solutions) {
+            for (String row : solution) {
+                System.out.println(row);
+            }
+            System.out.println();
+        }
+    }
+}
